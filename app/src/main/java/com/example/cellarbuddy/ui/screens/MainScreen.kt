4f7aa54be21938c003dc99fbea479dad1a64c246
n.kritikou <n.kritikou@epsilonnet.gr>
2025-09-29 09:28:26 +0300

    change in libs and hiltViewModel reference package

--- app/src/main/java/com/example/cellarbuddy/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/cellarbuddy/ui/screens/MainScreen.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -14,7 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.cellarbuddy.ui.components.BaseScreen
 import com.example.cellarbuddy.ui.components.ButtonLight
 import com.example.cellarbuddy.viewModels.MainViewModel
@@ -33,9 +31,6 @@ fun MainScreen(
     // doesn't do anything actually
     LaunchedEffect(Unit) {
         viewModel.loadOnce()
-
-        // test the snackbar
-        snackbarHostState.showSnackbar("testing snacks!")
     }
 
     MainContents(
